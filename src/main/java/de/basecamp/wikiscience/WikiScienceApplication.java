dd26c98856d5dac2e815c531a0eb81fda3ac6289
talbx <dev@talbx.de>
2020-02-17 15:08:12 +0100

    sample hadoop map/reducer setup

--- src/main/java/de/basecamp/wikiscience/WikiScienceApplication.java
+++ src/main/java/de/basecamp/wikiscience/WikiScienceApplication.java
@@ -1,13 +1,40 @@
 package de.basecamp.wikiscience;
 
+
+import de.basecamp.wikiscience.mapper.SampleMapper;
+import de.basecamp.wikiscience.reducer.SampleReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class WikiScienceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WikiScienceApplication.class, args);
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "wordcount");
+		// job.setJarByClass(WordCount.class);
+
+		job.setMapperClass(SampleMapper.class);
+		// optional combiner class
+		job.setCombinerClass(SampleReducer.class);
+		job.setReducerClass(SampleReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 
 }
