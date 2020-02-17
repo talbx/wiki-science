3984b919cdb0779e268d8df8d2b361e77164ce7c
talbx <dev@talbx.de>
2020-02-17 14:43:43 +0100

    start

---
+++ src/main/java/de/basecamp/wikiscience/WikiScienceApplication.java
@@ -0,0 +1,13 @@
+package de.basecamp.wikiscience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiScienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiScienceApplication.class, args);
+	}
+
+}
