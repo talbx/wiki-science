5b99447f1ad3be779d0c332a56fbd99ecd2ac770
talbx <dev@talbx.de>
2020-02-24 12:05:13 +0100

    jobfactory test

---
+++ src/test/java/org/unihh/basecamp/g4/wiki/JobFactoryTest.java
@@ -0,0 +1,31 @@
+package org.unihh.basecamp.g4.wiki;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.unihh.basecamp.g4.wiki.jobs.WikiJob;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JobFactoryTest {
+
+    @Test
+    public void shouldReturnJob() {
+        JobFactory factory = new JobFactory();
+        String sample = "word-count";
+        WikiJob wikiJob = factory.apply(sample);
+        Assertions.assertThat(wikiJob).isNotNull();
+        Assertions.assertThat(wikiJob.getName()).isEqualTo(sample);
+    }
+
+    @Test
+    public void should_fail() {
+        JobFactory factory = new JobFactory();
+        String sample = "sample";
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> factory.apply(sample))
+                .withMessageContaining("job could not be found");
+    }
+}
