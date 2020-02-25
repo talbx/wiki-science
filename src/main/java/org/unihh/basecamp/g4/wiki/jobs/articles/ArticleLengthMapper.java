7fc81f5c28be59f04b39e9eefd5ab81750ff2cb6
talbx <dev@talbx.de>
2020-02-25 11:45:32 +0100

    x

--- src/main/java/org/unihh/basecamp/g4/wiki/jobs/articles/ArticleLengthMapper.java
+++ src/main/java/org/unihh/basecamp/g4/wiki/jobs/articles/ArticleLengthMapper.java
@@ -1,5 +1,6 @@
 package org.unihh.basecamp.g4.wiki.jobs.articles;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -7,44 +8,19 @@ import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
-import org.w3c.dom.Document;
+import org.unihh.basecamp.g4.wiki.jobs.NodeBuilder;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.io.StringReader;
 
 public class ArticleLengthMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
-    private final IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
-        String line = value.toString();
-
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = null;
-
-        try {
-            dBuilder = dbFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Document doc = dBuilder.parse(new InputSource(new StringReader(line)));
-            Node node = doc.getDocumentElement();
-
-            processNode(node, output);
-
-        } catch (SAXException e) {
-            e.printStackTrace();
-        }
-
+        NodeBuilder nodeBuilder = new NodeBuilder();
+        Node node = nodeBuilder.apply(value);
+        processNode(node, output);
     }
 
     private void processNode(Node node, OutputCollector<Text, IntWritable> output) throws IOException {
@@ -52,8 +28,10 @@ public class ArticleLengthMapper extends MapReduceBase implements Mapper<LongWri
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node currentNode = nodeList.item(i);
             if (currentNode.getNodeName().equals("ns0:text")) {
-                word.set(currentNode.getTextContent());
-                output.collect(word, one);
+                String substringBefore = StringUtils.substringBefore(currentNode.getTextContent(), "==References==");
+                String clean = substringBefore.replaceAll("[^.,a-zA-Z ]", " ");
+                word.set(clean);
+                output.collect(word, new IntWritable(clean.length()));
             } else if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
                 processNode(currentNode, output);
             }
