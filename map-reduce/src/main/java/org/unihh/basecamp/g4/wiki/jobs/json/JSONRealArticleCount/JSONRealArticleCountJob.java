0029891993a7e360b2e3cb4fcbbe67289c00f104
talbx <dev@talbx.de>
2020-03-03 09:34:15 +0100

    yay

--- map-reduce/src/main/java/org/unihh/basecamp/g4/wiki/jobs/json/JSONRealArticleCount/JSONRealArticleCountJob.java
+++ map-reduce/src/main/java/org/unihh/basecamp/g4/wiki/jobs/json/JSONRealArticleCount/JSONRealArticleCountJob.java
@@ -1,17 +1,34 @@
 package org.unihh.basecamp.g4.wiki.jobs.json.JSONRealArticleCount;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.unihh.basecamp.g4.wiki.ConfGenerator;
 import org.unihh.basecamp.g4.wiki.jobs.WikiJob;
+import org.unihh.basecamp.g4.wiki.jobs.json.JSONTopics.JSONTopicsJob;
+import org.unihh.basecamp.g4.wiki.jobs.json.JSONTopics.JSONTopicsMapper;
+
+import java.io.IOException;
 
 public class JSONRealArticleCountJob implements WikiJob {
 
     private JobConf conf;
 
-    @Override
-    public void start(final String input, final String Output) {
-        ConfGenerator confGenerator = new ConfGenerator();
-        conf = confGenerator.generateTextIntConf("real-article-count-json", JSONRealArticleCountJob.class, JSONRealArticleCountMapper.class);
+    public JSONRealArticleCountJob() {
+        ConfGenerator generator = new ConfGenerator();
+        conf = generator.generateTextIntConf("real-article-count-json", JSONRealArticleCountJob.class, JSONRealArticleCountMapper.class);
+    }
+
+    public void start(String input, String output) {
+        try {
+            FileInputFormat.setInputPaths(conf, new Path(input));
+            FileOutputFormat.setOutputPath(conf, new Path(output));
+            JobClient.runJob(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
--- map-reduce/src/main/java/org/unihh/basecamp/g4/wiki/jobs/json/JSONTotalArticleCount/JSONTotalArticleCountJob.java
+++ map-reduce/src/main/java/org/unihh/basecamp/g4/wiki/jobs/json/JSONTotalArticleCount/JSONTotalArticleCountJob.java
@@ -1,17 +1,34 @@
 package org.unihh.basecamp.g4.wiki.jobs.json.JSONTotalArticleCount;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.unihh.basecamp.g4.wiki.ConfGenerator;
 import org.unihh.basecamp.g4.wiki.jobs.WikiJob;
+import org.unihh.basecamp.g4.wiki.jobs.json.JSONRedirects.JSONRedirectsJob;
+import org.unihh.basecamp.g4.wiki.jobs.json.JSONRedirects.JSONRedirectsMapper;
+
+import java.io.IOException;
 
 public class JSONTotalArticleCountJob implements WikiJob {
 
     private JobConf conf;
 
-    @Override
-    public void start(final String input, final String Output) {
-        ConfGenerator confGenerator = new ConfGenerator();
-        conf = confGenerator.generateTextIntConf("total-article-count-json", JSONTotalArticleCountJob.class, JSONTotalArticleCountMapper.class);
+    public JSONTotalArticleCountJob(){
+        ConfGenerator generator = new ConfGenerator();
+        conf = generator.generateTextIntConf("total-article-count-json", JSONTotalArticleCountJob.class, JSONTotalArticleCountMapper.class);
+    }
+
+    public void start(String input, String output) {
+        try {
+            FileInputFormat.setInputPaths(conf, new Path(input));
+            FileOutputFormat.setOutputPath(conf, new Path(output));
+            JobClient.runJob(conf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
